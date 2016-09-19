ac1942ad7ef4cdf7936699a09e8d3d27d66d2677
WatsonYao <yaoyuan131617@gmail.com>
2016-09-19 19:14:52 +0800

    update single test

--- app/src/main/java/demo/kotlin/mathilda/love/watson/watsondemo/MainActivity.kt
+++ app/src/main/java/demo/kotlin/mathilda/love/watson/watsondemo/MainActivity.kt
@@ -8,17 +8,29 @@ import android.support.v7.widget.Toolbar
 import android.view.View
 import android.view.Menu
 import android.view.MenuItem
+import rx.Single
+import rx.SingleSubscriber
+import rx.Subscriber
+import rx.android.schedulers.AndroidSchedulers
+import rx.functions.Func1
+import rx.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
 
         val fab = findViewById(R.id.fab) as FloatingActionButton
-        fab.setOnClickListener { view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show() }
+        fab.setOnClickListener {
+            view ->
+            test3()
+            //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show()
+        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -39,5 +51,62 @@ class MainActivity : AppCompatActivity() {
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+    fun test() {
+        Single.just(2).subscribe(object : SingleSubscriber<Int>() {
+            override fun onSuccess(value: Int?) {
+                // value = 3
+                MyLoggerK.create().i("value->" + value)
+            }
+
+            override fun onError(error: Throwable) {
+            }
+        })
+    }
+
+    fun test2() {
+        Single.just(3).compose(object : Single.Transformer<Int, String> {
+            override fun call(integerSingle: Single<Int>): Single<String> {
+                return integerSingle.map(object : Func1<Int, String> {
+                    override fun call(integer: Int?): String {
+                        return (integer!! + 2).toString()
+                    }
+                })
+            }
+        }).subscribe(object : SingleSubscriber<String>() {
+            override fun onSuccess(value: String) {
+                // value = 5
+                MyLoggerK.create().i("value->" + value)
+            }
+
+            override fun onError(error: Throwable) {
+            }
+        })
+    }
+
+    fun test3() {
+
+        // 常见的示例，这是一个异步操作
+        Single.create(object : Single.OnSubscribe<Int> {
+            override fun call(singleSubscriber: SingleSubscriber<in Int>) {
+                // 这里被指定在IO线程
+                singleSubscriber.onSuccess(3)
+            }
+        })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(object : Subscriber<Int>() {
+                    override fun onCompleted() {
+                    }
+
+                    override fun onError(e: Throwable) {
+                    }
+
+                    override fun onNext(o: Int?) {
+                        // o = 3
+                        MyLoggerK.create().i("o->" + o)
+                    }
+                })
     }
 }
---
+++ app/src/main/java/demo/kotlin/mathilda/love/watson/watsondemo/MyLoggerK.kt
@@ -0,0 +1,176 @@
+package demo.kotlin.mathilda.love.watson.watsondemo
+
+/**
+ * Created by watson on 16/9/19.
+ */
+import android.util.Log
+
+/**
+ * 调试日志，发布正式版本时，请将logFlag置位成false
+ *
+ * @author Administrator
+ */
+class MyLoggerK private constructor(name: String) {
+
+    private val logFlag = true
+    val tag = "kwapp"
+    private val logLevel = Log.VERBOSE
+    private var mClassName: String = ""
+
+    init {
+        mClassName = name
+    }
+
+    companion object {
+        fun create(): MyLoggerK = MyLoggerK("@")
+    }
+
+
+    /**
+     * Get The Current Function Name
+     *
+     * @return
+     */
+    private fun getFunctionName(): String? {
+        var sts = Thread.currentThread().stackTrace
+
+        if (sts == null) {
+            return null
+        }
+
+        for (st in sts) {
+//
+
+            if (st.isNativeMethod) {
+                continue
+            }
+
+            if (st.className.equals(Thread::class.java.name)) {
+                continue
+            }
+            if (st.className.equals(this.javaClass.name)) {
+                continue
+            }
+
+            return mClassName + "[ " + Thread.currentThread().name + ": " + st.fileName + ":" + st.lineNumber + " " + st.methodName + " ]"
+        }
+        return null
+    }
+
+    /**
+     * The Log Level:i
+     *
+     * @param str
+     */
+    fun i(str: Any) {
+        if (logFlag) {
+            if (logLevel <= Log.INFO) {
+                val name = getFunctionName()
+                if (name != null) {
+                    Log.i(tag, name + " - " + str)
+                } else {
+                    Log.i(tag, str.toString())
+                }
+            }
+        }
+
+    }
+
+    /**
+     * The Log Level:d
+     *
+     * @param str
+     */
+    fun d(str: Any) {
+        if (logFlag) {
+            if (logLevel <= Log.DEBUG) {
+                val name = getFunctionName()
+                if (name != null) {
+                    Log.d(tag, name + " - " + str)
+                } else {
+                    Log.d(tag, str.toString())
+                }
+            }
+        }
+    }
+
+    /**
+     * The Log Level:V
+     *
+     * @param str
+     */
+    fun v(str: Any) {
+        if (logFlag) {
+            if (logLevel <= Log.VERBOSE) {
+                val name = getFunctionName()
+                if (name != null) {
+                    Log.v(tag, name + " - " + str)
+                } else {
+                    Log.v(tag, str.toString())
+                }
+            }
+        }
+    }
+
+    /**
+     * The Log Level:w
+     *
+     * @param str
+     */
+    fun w(str: Any) {
+        if (logFlag) {
+            if (logLevel <= Log.WARN) {
+                val name = getFunctionName()
+                if (name != null) {
+                    Log.w(tag, name + " - " + str)
+                } else {
+                    Log.w(tag, str.toString())
+                }
+            }
+        }
+    }
+
+    /**
+     * The Log Level:e
+     *
+     * @param str
+     */
+    fun e(str: Any) {
+        if (logFlag) {
+            if (logLevel <= Log.ERROR) {
+                val name = getFunctionName()
+                if (name != null) {
+                    Log.e(tag, name + " - " + str)
+                } else {
+                    Log.e(tag, str.toString())
+                }
+            }
+        }
+    }
+
+    /**
+     * The Log Level:e
+     *
+     * @param ex
+     */
+    fun e(ex: Exception) {
+        if (logFlag) {
+            if (logLevel <= Log.ERROR) {
+                Log.e(tag, "error", ex)
+            }
+        }
+    }
+
+    /**
+     * The Log Level:e
+     *
+     * @param log
+     * @param tr
+     */
+    fun e(log: String, tr: Throwable) {
+        if (logFlag) {
+            val line = getFunctionName()
+            Log.e(tag, "{Thread:" + Thread.currentThread().name + "}" + "[" + mClassName + line + ":] " + log + "\n", tr)
+        }
+    }
+}
